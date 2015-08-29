568ad386c2f616baa0d46e5c9d4a12daa460f3aa
Mark Repka <mark@repkam09.com>
2015-08-29 16:01:28 -0400

    Add getTemp

--- client/client.js
+++ client/client.js
@@ -1,41 +1,34 @@
 var fs = require('fs');
 var https = require('https');
-
 var Cron = require('cron').CronJob;
-var ADC = require('../adc-pi-gpio');
 
-var config = {
-    tolerance: 2,
-    interval: 300,
-    channels: [0],
-    SPICLK: 12,
-    SPIMISO: 16,
-    SPIMOSI: 18,
-    SPICS: 22
+function getTemp() {
+    fs.readFile('/sys/bus/w1/devices/28-00000400a88a/w1_slave', function (err, buffer) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+
+        // Read data from file (using fast node ASCII encoding).
+        var data = buffer.toString('ascii').split(" "); // Split by space
+
+        // Extract temperature from string and divide by 1000 to give celsius
+        var temp = parseFloat(data[data.length - 1].split("=")[1]) / 1000.0;
+
+        // Round to one decimal place
+        temp = Math.round(temp * 10) / 10;
+
+        // Add date/time to temperature
+        var response = {
+            unix_time: Date.now(),
+            c: temp,
+            f: (temp * 9 / 5 + 32)
+        };
+
+        // Execute call back with data
+        return response;
+    });
 };
-
-
-var adc = new ADC(config);
-
-process.on('SIGTERM', function () {
-    adc.close();
-});
-process.on('SIGINT', function () {
-    adc.close();
-});
-
-adc.init();
-
-adc.on('ready', function () {
-    console.log('Pins ready, listening to channel');
-});
-adc.on('close', function () {
-    console.log('ADC terminated');
-    process.exit();
-});
-adc.on('change', function (data) {
-    console.log('Channel ' + data.channel + ' value is now ' + data.value + ' which in proportion is: ' + data.percent);
-});
 
 
 
@@ -43,9 +36,11 @@ var tempcheck = function () {
     console.log("client.js - tempcheck - start");
     var temp = getTemp();
 
+    console.log(JSON.stringify(temp));
+
     var options = {
         host: 'https://repkam09.com',
-        path: '/repserv/tempmon/' + temp
+        path: '/repserv/tempmon/' + temp.f
     };
 
     var req = https.get(options, function (res) {
@@ -73,11 +68,5 @@ var onFinish = function () {
     console.log("client.js - onFinish - start");
 };
 
-function getTemp() {
-    console.log("client.js - getTemp from gpio");
-    return 70; // This is TOTALLY gpio. Really.
-}
-
 // Start a new cron task
-// constructor(cronTime, onTick, onComplete, start, timezone, context)
 var task = new Cron('0 */2 * * *', tempcheck, onFinish, true, null, null);
